d27e3cc8ac06229a4e89d24565a81d9f7e7c81b0
Jordan Rogers <fbrogers@gmail.com>
2013-01-11 09:49:30 -0500

    retooling, cleanup

--- js/global.js
+++ js/global.js
@@ -1,26 +1,25 @@
 $(function(){
-	$('nav > ul > li > a').click(function(event){
-		$(this).next('ul').not('.ignore').slideToggle('fast');
+	$('nav > ul > li[class!="ignore"] > a').click(function(event){
+		$(this).next('ul:first').slideToggle('fast');
 		event.preventDefault();
 	});
 
 	$('#change-bg').click(function(event){
-		/*$('#background, body').css({
-			'background-image': 'url(images/backgrounds/'+(Math.floor(Math.random()*max)+min)+'.jpg)'
-		});*/
+		//range and random choice
+		var rand = Math.floor(Math.random()*70)+1;
 
-		var min = 1;
-		var max = 70;
-		var rand = Math.floor(Math.random()*max)+min;
-
-		$('#background').animate({opacity: 0}, 'fast', function() {
+		//animate the background change by fading out the old and fading in the new
+		$('#background').animate({ opacity: 0 }, 'fast', function(){
 			$(this).css({
 				'background-image': 'url(images/backgrounds/' + rand + '.jpg)'
-			}).animate({
-				opacity: 1
-			});
+			}).animate({ opacity: 1 });
 		});
 
+		event.preventDefault();
+	});
+
+	//prevent blanks from firing
+	$('.ignore a[href="#"]').click(function(){
 		event.preventDefault();
 	});
 });
