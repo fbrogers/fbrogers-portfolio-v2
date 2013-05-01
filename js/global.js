d615a41d74604640ecdbae6fe1d4bc43eb670082
Jordan Rogers <fbrogers@gmail.com>
2013-05-01 02:24:55 -0400

    cleanup

--- js/global.js
+++ js/global.js
@@ -1,12 +1,7 @@
 $(function(){
-	$('nav > ul > li[class!="ignore"] > a').click(function(event){
-		$(this).next('ul:first').slideToggle('fast');
-		event.preventDefault();
-	});
-
-	$('#change-bg').click(function(event){
+	function changeBackground(){
 		//range and random choice
-		var rand = Math.floor(Math.random()*70)+1;
+		var rand = Math.floor(Math.random()*10)+1;
 
 		//animate the background change by fading out the old and fading in the new
 		$('#background').animate({ opacity: 0 }, 'fast', function(){
@@ -14,7 +9,18 @@ $(function(){
 				'background-image': 'url(images/backgrounds/' + rand + '.jpg)'
 			}).animate({ opacity: 1 });
 		});
+		
+		$('body').css('background-image', 'url(images/backgrounds/' + rand + '.jpg)');
+	}
+	changeBackground();
 
+	$('nav > ul > li[class!="ignore"] > a').click(function(event){
+		$(this).next('ul:first').slideToggle('fast');
+		event.preventDefault();
+	});
+
+	$('#change-bg').click(function(event){
+		changeBackground();
 		event.preventDefault();
 	});
 
@@ -22,4 +28,29 @@ $(function(){
 	$('.ignore a[href="#"]').click(function(){
 		event.preventDefault();
 	});
+	
+	//preload bg images
+	if(document.images){
+		img1 = new Image();
+		img2 = new Image();
+		img3 = new Image();
+		img4 = new Image();
+		img5 = new Image();
+		img6 = new Image();
+		img7 = new Image();
+		img8 = new Image();
+		img9 = new Image();
+		img10 = new Image();
+
+		img1.src = "images/backgrounds/1.jpg";
+		img2.src = "images/backgrounds/2.jpg";
+		img3.src = "images/backgrounds/3.jpg";
+		img4.src = "images/backgrounds/4.jpg";
+		img5.src = "images/backgrounds/5.jpg";
+		img6.src = "images/backgrounds/6.jpg";
+		img7.src = "images/backgrounds/7.jpg";
+		img8.src = "images/backgrounds/8.jpg";
+		img9.src = "images/backgrounds/9.jpg";
+		img10.src = "images/backgrounds/10.jpg";
+	}
 });
